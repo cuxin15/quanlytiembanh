67a4c984e83c7e53d1a82b52b31b29d578ea9fbf
truongaxin123 <truongaxin123@gmail.com>
2020-10-22 07:56:30 +0700

    edit program

--- QuanLyTiemBanh/Program.cs
+++ QuanLyTiemBanh/Program.cs
@@ -17,6 +17,7 @@ namespace QuanLyTiemBanh
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
 			Application.Run(new frm_NguyenLieu());
+//truongaxin
 		}
 	}
 }
